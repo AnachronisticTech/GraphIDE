507e8364371e18470eb28e9bf9dacfa053218108
Daniel <danielmarriner@anachronistic-tech.co.uk>
2021-10-23 22:04:00 +0300

    Updated `JavaAnalyzer` to output classes

--- source/main.ts
+++ source/main.ts
@@ -1,10 +1,7 @@
-import {ProjectAnalyzer} from "./ProjectAnalyzer";
 import { JavaAnalyzer } from "./Analyzers"
 
-function add(x: number, y: number ) : number {
-    return x + y;
-}
-
-console.log(add(2,3))
-// ProjectAnalyzer.readSourceDirectory("FizzBuzzProject", true)
-JavaAnalyzer.analyze("FizzBuzzProject")
+let analyzer = new JavaAnalyzer()
+analyzer.analyze("FizzBuzzProject", () => {
+    console.log("OUTPUT:");
+    console.log(analyzer.classes)
+})
